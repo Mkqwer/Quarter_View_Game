b7b4f7be5689343841ade19623cd187cb0e0bc45
Mkqwer <meebk2@naver.com>
2025-04-05 22:57:00 +0900

    Add Equip/SwapWeaponLogic
    영상 4 완료

--- 3DActionGame/Assets/Script/Player.cs
+++ 3DActionGame/Assets/Script/Player.cs
@@ -4,14 +4,23 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    public float PlayerSpeed;
+    public float PlayerSpeed;       
+    public GameObject[] Weapons;   
+    public bool[] HasWeapons;       
+
     float hAxis;
     float vAxis;
 
     bool WalkDown;
     bool JumpDown;
+    bool iDown;
+    bool SwapDown1;
+    bool SwapDown2;
+    bool SwapDown3;
+
     bool IsJump;
     bool IsDodge;
+    bool IsSwap;
 
     bool IsSide; //벽 충돌 유무
     Vector3 SideVec; //벽 충돌 방향 저장
@@ -20,7 +29,10 @@ public class Player : MonoBehaviour
 
     Rigidbody Rigid;
     Animator Anim;
-
+    
+    GameObject NearObject;
+    GameObject EquipWeapon;
+    int EquipWaeponIndex = -1;
     void Awake()
     {
         Rigid = GetComponent<Rigidbody>();
@@ -35,6 +47,8 @@ public class Player : MonoBehaviour
         Turn();
         Jump();
         Dodge();
+        Swap();
+        Interation();
     }
     
     void GetInput()
@@ -43,6 +57,11 @@ public class Player : MonoBehaviour
         vAxis = Input.GetAxisRaw("Vertical");
         WalkDown = Input.GetButton("Walk");
         JumpDown = Input.GetButtonDown("Jump");
+        iDown = Input.GetButtonDown("Interation");
+        SwapDown1 = Input.GetButtonDown("Swap1");
+        SwapDown2 = Input.GetButtonDown("Swap2");
+        SwapDown3 = Input.GetButtonDown("Swap3");
+
     }
 
     void Move()
@@ -51,6 +70,9 @@ public class Player : MonoBehaviour
 
         if (IsDodge)
             MoveVec = DodgeVec;
+
+        if (IsSwap)
+            MoveVec = Vector3.zero;
 
         //충돌하는 방향은 무시
         if (IsSide && MoveVec == SideVec)
@@ -72,7 +94,7 @@ public class Player : MonoBehaviour
    
     void Jump()
     {
-        if (JumpDown && MoveVec == Vector3.zero && !IsJump && !IsDodge) 
+        if (JumpDown && MoveVec == Vector3.zero && !IsJump && !IsDodge && !IsSwap) 
         {
             Rigid.AddForce(Vector3.up * 25, ForceMode.Impulse);
             Anim.SetBool("IsJump", true);
@@ -82,7 +104,7 @@ public class Player : MonoBehaviour
     }
     void Dodge()
     {
-        if (JumpDown && MoveVec != Vector3.zero && !IsJump && !IsDodge)
+        if (JumpDown && MoveVec != Vector3.zero && !IsJump && !IsDodge && !IsSwap)
         {
             DodgeVec = MoveVec;
             PlayerSpeed *= 2;
@@ -99,6 +121,59 @@ public class Player : MonoBehaviour
         IsDodge = false;
     }
 
+    void Swap()
+    {
+        if (SwapDown1 && (!HasWeapons[0] || EquipWaeponIndex == 0)) 
+            return;
+        if (SwapDown2 && (!HasWeapons[1] || EquipWaeponIndex == 1)) 
+            return;
+        if (SwapDown3 && (!HasWeapons[2] || EquipWaeponIndex == 2)) 
+            return;
+  
+        int WeaponIndex = -1;
+       
+        if (SwapDown1) WeaponIndex = 0;
+        if (SwapDown2) WeaponIndex = 1;
+        if (SwapDown3) WeaponIndex = 2;
+        
+
+        if ((SwapDown1 || SwapDown2 || SwapDown3) && !IsJump && !IsDodge)
+        {
+            if(EquipWeapon != null)
+            {               
+                EquipWeapon.SetActive(false);
+            }
+
+            EquipWaeponIndex = WeaponIndex;
+            EquipWeapon = Weapons[WeaponIndex];
+            EquipWeapon.SetActive(true);
+            
+            Anim.SetTrigger("DoSwap");
+
+            IsSwap = true;
+
+            Invoke("SwapOut", 0.4f);
+        }
+    }
+
+    void SwapOut()
+    {
+        IsSwap = false;
+    }
+    void Interation()
+    {
+        if (iDown && NearObject != null && !IsJump && !IsDodge)
+        {
+            if (NearObject.tag == "Weapon")
+            {
+                Item item = NearObject.GetComponent<Item>();
+                int WeaponIndex = item.Value;
+                HasWeapons[WeaponIndex] = true;
+
+                Destroy(NearObject);
+            }
+        }
+    }
     void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.tag == "Floor")
@@ -126,4 +201,22 @@ public class Player : MonoBehaviour
         }
     }
 
+    void OnTriggerStay(Collider other)
+    {
+        if(other.tag == "Weapon")
+        {            NearObject = other.gameObject;
+
+        Debug.Log(NearObject.name);
+        }
+
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Weapon") 
+        { 
+            NearObject = null;
+        }
+
+    }
 }
